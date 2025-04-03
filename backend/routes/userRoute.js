d25d742abc3c9b8400abd6be9e7372d4fd368412
Chan Meng Ong <ongchanmeng@nipponpaint.com.my>
2025-04-03 19:59:02 +0800

    add update current user info

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUser, logoutUser, getAllUsers, getCurrentUserProfile } from "../controllers/userController.js";
+import { createUser, loginUser, logoutUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth', loginUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(authenticate, getCurrentUserProfile)
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile)
 
 export default router;
